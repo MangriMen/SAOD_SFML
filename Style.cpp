1cea7abc2b38b9a990f403115e8ad26fd5552239
Igor <49300253+MangriMen@users.noreply.github.com>
2020-03-09 02:19:53 +0700

    updated .gitignore

--- Style.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <thread>
-#include <sstream>
-#include <SFML/Graphics.hpp>
-#include "SelectSort.hpp"
-#include "BubbleSort.hpp"
-#include "ShakerSort.hpp"
-#include "compare.hpp"
-
